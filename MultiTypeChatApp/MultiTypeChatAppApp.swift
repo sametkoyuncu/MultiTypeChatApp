2e392cd5cdbaeb676bf5b418b1afd88dd1cdfef3
Samet Koyuncu <sametkoyuncu@live.com>
2025-12-06 07:32:40 +0300

    Initial Commit

---
+++ MultiTypeChatApp/MultiTypeChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiTypeChatAppApp.swift
+//  MultiTypeChatApp
+//
+//  Created by Samet Koyuncu on 6.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct MultiTypeChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
